44c8b88ecbda3b0af30e5745e1c61ac216d19d17
gosungjinmoon <55925545+gosungjinmoon@users.noreply.github.com>
2025-10-14 16:31:01 +0900

    Update theme-config.js

--- config/theme-config.js
+++ config/theme-config.js
@@ -1,1 +1,5 @@
-window.__GFW_THEME__ = { default: "light", activeTheme: "forest", allowUserToggle: false, allowLangToggle: true };
+window.themeConfig = {
+  current: "light",
+  available: ["light","dark","mint","sunset"],
+  updated: "2025-10-14T06:42:19.143431"
+};
