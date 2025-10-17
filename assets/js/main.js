3f2b1f12697dfce0c981cfef1e33c03b7f74fad5
gosungjinmoon <55925545+gosungjinmoon@users.noreply.github.com>
2025-10-17 14:17:59 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,2 @@
-/* assets/js/main.js  버전 정보_202510171235 */
-document.addEventListener("DOMContentLoaded", () => {
-  const yearEl = document.getElementById("year");
-  if (yearEl) yearEl.textContent = new Date().getFullYear();
-});
+/* assets/js/main.js  버전 정보_202510171345 */
+document.addEventListener("DOMContentLoaded",()=>{const y=document.getElementById("year");if(y)y.textContent=new Date().getFullYear();});
