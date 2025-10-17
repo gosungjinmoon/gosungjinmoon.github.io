4b4614b6399872c6ef0d042dd3cbd0731a5bad77
gosungjinmoon <55925545+gosungjinmoon@users.noreply.github.com>
2025-10-17 14:18:49 +0900

    Update translator.js

--- assets/js/translator.js
+++ assets/js/translator.js
@@ -1,6 +1,2 @@
-/* assets/js/translator.js  버전 정보_202510171235 */
-(function () {
-  const html = document.documentElement;
-  const lang = html.getAttribute("lang") || "ko";
-  console.log("[translator] current lang:", lang);
-})();
+/* assets/js/translator.js  버전 정보_202510171345 */
+(function(){const html=document.documentElement;const lang=html.getAttribute("lang")||(window.__GFW_LANGS__&&__GFW_LANGS__.default)||"ko";console.log("[translator] current lang:",lang);})();
