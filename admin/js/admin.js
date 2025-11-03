d2166753da80f40e33ece3cd0cc0ff945fe88ec5
gosungjinmoon <55925545+gosungjinmoon@users.noreply.github.com>
2025-11-03 13:56:47 +0900

    Create admin.js

---
+++ admin/js/admin.js
@@ -0,0 +1,4 @@
+/* /admin/js/admin.js v202511032300 */
+const GITHUB_CLIENT_ID = "Ov23liqsljSBAqajz2eu";
+const N8N_NEW_POST_URL = "https://n8n.gofunwith.com/webhook/new-post";
+// ... (이하 OAuth, 폼 제출, n8n 호출 로직)
