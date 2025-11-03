31f79af05c1f865df0d035713afb7623b304a1c3
gosungjinmoon <55925545+gosungjinmoon@users.noreply.github.com>
2025-11-03 17:32:52 +0900

    Create custom.js

---
+++ assets/js/dist/custom.js
@@ -0,0 +1,8 @@
+/* /assets/js/dist/custom.js v202511050100 */
+
+// Disable Turbo to fix all navigation and script reload issues.
+// This is the definitive solution for the "Ctrl+Shift+R" problem.
+// This file is automatically loaded by the Chirpy theme.
+if (typeof Turbo !== 'undefined') {
+  Turbo.session.drive = false;
+}
