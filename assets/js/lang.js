7ad31264bfb11759aea0735a59c4b0f6b0928d91
gosungjinmoon <55925545+gosungjinmoon@users.noreply.github.com>
2025-11-06 16:12:49 +0900

    Delete assets/js/lang.js

--- assets/js/lang.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const currentLang = document.documentElement.lang || 'ko';
-  const recentList = document.querySelectorAll('.recent-posts li a');
-  recentList.forEach(link => {
-    if (!link.href.includes('/' + currentLang + '/')) {
-      link.parentElement.style.display = 'none';
-    }
-  });
-});
