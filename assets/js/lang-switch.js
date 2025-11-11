b83b6bc72e6e0b440f73d1bda8ad305dc24e8119
gosungjinmoon <55925545+gosungjinmoon@users.noreply.github.com>
2025-11-11 14:51:46 +0900

    Delete assets directory

--- assets/js/lang-switch.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  var sel = document.getElementById('lang-switch');
-  if(!sel) return;
-  sel.addEventListener('change', function(e){
-    var lang = e.target.value;
-    var path = location.pathname.replace(/^\/(ko|en)\//, '/');
-    location.href = '/' + lang + path;
-  });
-})();
